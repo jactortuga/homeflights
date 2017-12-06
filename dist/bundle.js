a9f713be59f6917e044c5a70bfdba4aebd5102db
jactortuga <jac@tortugalabs.io>
2017-12-06 00:32:37 +0000

    data service commented and further refactored

--- dist/bundle.js
+++ dist/bundle.js
@@ -80448,18 +80448,23 @@ angular
             var dataFull      = [];
             var dataFinal     = {};
 
-            // Split data content based on comma and push into array as single set of flight information
+            // Loop through data array of individual flights information strings
             for (var i = 0; i < dataArray.length; i++) {
+              // Split individual flight information string into array to access and store values
               var individualTrip = dataArray[i].split(',');
+              // Check for new individual flight information array length against data headers to avoid corrupted data
               if (individualTrip.length === dataHeader.length) {
+                // Within each individual flight information array, loop through it to access individual values
                 for (var j = 0; j < dataHeader.length; j++) {
+                  // First array represents data header, thus create working object and push into working array
                   if(i === 0) {
                     var dataHeaderObject = {
                       type:	dataHeader[j],
                       recurrenceObject: {},
-                      recurrenceArray:	[]
+                      recurrenceArray: []
                     };
                     dataFull.push(dataHeaderObject);
+                  // For remaining arrays, check recurrence and set value in recurrence object within working array
                   } else {
                     if(!dataFull[j].recurrenceObject[individualTrip[j]]) {
                       dataFull[j].recurrenceObject[individualTrip[j]] = 0;
@@ -80470,14 +80475,18 @@ angular
               }
             }
 
+            // Loop through working array to get recurrence values and define final array in desired format
             for (var p = 0; p < dataFull.length; p++) {
+              // Loop through recurrence object to get recurrence name and score
               for (var key in dataFull[p].recurrenceObject) {
                 if (dataFull[p].recurrenceObject.hasOwnProperty(key)) {
+                  // Push selected key and value in recurrence array
                   var recurrenceSubObject = {
                     key: key,
                     value: dataFull[p].recurrenceObject[key]
                   };
                   dataFull[p].recurrenceArray.push(recurrenceSubObject);
+                  // Get array element type value and recurrence array and push them into final array
                   var dataType        = dataFull[p].type;
                   var dataRecurrence  = dataFull[p].recurrenceArray;
                   dataFinal[dataType] = dataRecurrence;
@@ -80485,16 +80494,15 @@ angular
               }
             }
 
-            // Return final set of data
-            console.log('DATA FINAL')
-            console.log(dataFinal)
+            // Return final set of data in selected format for easier manipulation and d3 access
             return dataFinal;
 
           // Define error callback functionality to display error
           }, function errorCallback(error) {
             console.log('Error [Service]');
             console.log(error);
-          });
+          }
+        );
       }
     };
   }]);
