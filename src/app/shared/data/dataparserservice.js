c7dc4fed4861ba800deb30d1f00986b568097b43
jactortuga <jac@tortugalabs.io>
2017-12-06 00:14:51 +0000

    data service refactored

--- src/app/shared/data/dataparserservice.js
+++ src/app/shared/data/dataparserservice.js
@@ -1,80 +1,70 @@
 angular
-.module('HomeFlights.dataParser', [])
-.factory('dataParserService', ['$http', function($http){
-  return {
-    getData: function() {
-      return $http.get('./assets/dataset/home_office_air_travel_data_2011.csv')
-        .then(function(response) {
-          console.log('Success [Service]');
-          console.log(response);
-          //return data when promise resolved
-          //that would help you to continue promise chain.
+  .module('HomeFlights.dataParser', [])
+  .factory('dataParserService', ['$http', function($http){
+    return {
+      // Define function to retrieve and manipulate data from dataset through $http
+      // TODO: split functionality in separate functions, optimise loops and store data accordingly
+      getData: function() {
+        return $http.get('./assets/dataset/home_office_air_travel_data_2011.csv')
+          // Define success callback functionality to return data when promise resolved
+          .then(function(response) {
+            console.log('Success [Service]');
+            console.log(response);
 
-          var dataRaw 			= response.data;
-          var dataArray 		= dataRaw.split(/\r\n|\n/);
-          var dataHeader 		= dataArray[0].split(',');
-          var dataClean 		= [];
-          var dataFull      = [];
-          var dataFinal     = {};
+            // Define functionality variables, data content array and data header array
+            var dataRaw 			= response.data;
+            var dataArray 		= dataRaw.split(/\r\n|\n/);
+            var dataHeader 		= dataArray[0].split(',');
+            var dataFull      = [];
+            var dataFinal     = {};
 
-          // split content based on comma
-          for (var i = 1; i < dataArray.length; i++) {
-            var data = dataArray[i].split(',');
-            if (data.length === dataHeader.length) {
-              var subArray = [];
-              for (var j = 0; j < dataHeader.length; j++) {
-                subArray.push(data[j]);
-              }
-              dataClean.push(subArray);
-            }
-          }
-
-          for (var k = 0; k < dataHeader.length; k++) {
-            var headerObject = {
-              type:	dataHeader[k],
-              data:	[],
-              recurrence: []
-            };
-            dataFull.push(headerObject);
-          }
-
-          for (var l = 0; l < dataClean.length; l++) {
-            for (var m = 0; m < dataClean[l].length; m++) {
-              dataFull[m].data.push(dataClean[l][m]);
-            }
-          }
-
-          for (var n = 0; n < dataFull.length; n++) {
-            var recurrenceTempObject = {};
-            for(var o = 0; o < dataFull[n].data.length; ++o) {
-              if(!recurrenceTempObject[dataFull[n].data[o]]) {
-                recurrenceTempObject[dataFull[n].data[o]] = 0;
-              }
-              ++recurrenceTempObject[dataFull[n].data[o]];
-            }
-
-            for (var key in recurrenceTempObject) {
-              if (recurrenceTempObject.hasOwnProperty(key)) {
-                var recurrenceSubObject = {
-                  key: key,
-                  value: recurrenceTempObject[key]
-                };
-                dataFull[n].recurrence.push(recurrenceSubObject);
+            // Split data content based on comma and push into array as single set of flight information
+            for (var i = 0; i < dataArray.length; i++) {
+              var individualTrip = dataArray[i].split(',');
+              if (individualTrip.length === dataHeader.length) {
+                for (var j = 0; j < dataHeader.length; j++) {
+                  if(i === 0) {
+                    var dataHeaderObject = {
+                      type:	dataHeader[j],
+                      recurrenceObject: {},
+                      recurrenceArray:	[]
+                    };
+                    dataFull.push(dataHeaderObject);
+                  } else {
+                    if(!dataFull[j].recurrenceObject[individualTrip[j]]) {
+                      dataFull[j].recurrenceObject[individualTrip[j]] = 0;
+                    }
+                    ++dataFull[j].recurrenceObject[individualTrip[j]];
+                  }
+                }
               }
             }
-          }
 
-          for (var p = 0; p < dataFull.length; p++) {
-            var dataType        = dataFull[p].type;
-            var dataRecurrence  = dataFull[p].recurrence;
-            dataFinal[dataType] = dataRecurrence;
-          }
+            for (var p = 0; p < dataFull.length; p++) {
+              for (var key in dataFull[p].recurrenceObject) {
+                if (dataFull[p].recurrenceObject.hasOwnProperty(key)) {
+                  var recurrenceSubObject = {
+                    key: key,
+                    value: dataFull[p].recurrenceObject[key]
+                  };
+                  dataFull[p].recurrenceArray.push(recurrenceSubObject);
+                  var dataType        = dataFull[p].type;
+                  var dataRecurrence  = dataFull[p].recurrenceArray;
+                  dataFinal[dataType] = dataRecurrence;
+                }
+              }
+            }
 
-          return dataFinal;
-        }, function errorCallback(error) {
-          console.log('Error [Service]');
-          console.log(error);
-        });
-    }
-  };
-}]);
+            // Return final set of data
+            console.log('DATA FINAL')
+            console.log(dataFinal)
+            return dataFinal;
+
+          // Define error callback functionality to display error
+          }, function errorCallback(error) {
+            console.log('Error [Service]');
+            console.log(error);
+          });
+      }
+    };
+  }]);
